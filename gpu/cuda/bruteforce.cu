3f0b795881e559f7545c58f89d0feef3e9366e94
djebm2 <djebm2@gmail.com>
2014-11-03 23:46:59 +0100

    Added comments to bruteforce.cu

--- gpu/cuda/bruteforce.cu
+++ gpu/cuda/bruteforce.cu
@@ -1,31 +1,47 @@
-__global__ void gpuDistances(float *A, float *B , float *D, int n, int m , int dim)
+template <typename TYPE>
+__global__ void bruteforce_distances (TYPE *A, TYPE *B , TYPE *D, int n, int m , int dim)
 {
-#if (__CUDA_ARCH__ >= 500)      // On Maxwell architecture (cc5.0 or newer)
-  #define CHUNK 16
-#elif (__CUDA_ARCH__ < 500)
+
+/*
+ * - Input arguments:  Matrix A[dim][m] (Transposed form | with cuBLAS or low level kernel).
+ *                     Matrix B[n][dim] (Original form).
+ * - Output arguments: Matrix D [m][n] (Transposed form, but must be reshaped to original form after kernel execution).
+ * 
+ * - This kernel computes the distances D between the n dim-sized vectors of A and the m dim-sized vectors of B.  
+ * - It has been tested on GPUs with cc 2.0, 2.1, 3.0 and 5.0 with TYPE = <float , int>
+ * - To have a better performance in your platform, you will have to jimmy the CHUNK size.
+ * 
+ * Algorithms:
+ * 1 - 
+ */
+
+#if (__CUDA_ARCH__ >= 500)      // Maxwell or newer
+  #define CHUNK 16 
+#elif (__CUDA_ARCH__ < 500)	// Kepler or older 
   #define CHUNK 4
 #endif
 
-extern __shared__ float sB[];       // Dynamic allocation
-int bx  = blockIdx.x;
+extern __shared__ TYPE sB[];    // Allocation of shared mem @ compile time : < CHUNK * sizeof(TYPE) >
 
-  int tx  = threadIdx.x;
-  for (int i = 0; i < CHUNK; i++)
+int bx  = blockIdx.x;		// Grid size  = < ceil(M/CHUNK) >
+int tx  = threadIdx.x;          // Block size = < 128 >
+  
+for (int i = 0; i < CHUNK; i++)
     sB[(i*dim)+tx] = B[(((bx*CHUNK)+i)*dim)+tx];
   __syncthreads();
 
   while (tx < n)
   {
-    float result[CHUNK];
+    TYPE result[CHUNK];
     for (int i = 0; i < CHUNK; i++)   // Initializing result array
-      result[i] = 0.0f;
+      result[i] = (TYPE) 0.0f;
 
     for (int i = 0; i < dim; i++)
     {
-      float Atemp = A[(n*i)+tx];    // Reading elements in array A for L2 cache storage
+      TYPE Atemp = A[(n*i)+tx];    // Reading elements of Matrix A for L1/L2 cache storage
       for (int j = 0; j < CHUNK; j++)
       {
-        float temp = Atemp - sB[i + (j*dim)];
+        TYPE temp = Atemp - sB[i + (j*dim)];
         result[j] += temp * temp;
       }
     }
